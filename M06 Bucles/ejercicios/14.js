ec17580c41787b73b99d0dd00118d6e57b2d0b5a
Luis <looes1@gmail.com>
2024-01-16 19:06:29 -0500

    ❤️🧠🚀✨

--- M06 Bucles/ejercicios/14.js
+++ M06 Bucles/ejercicios/14.js
@@ -1,11 +1,60 @@
 function operadoresLogicos(num1, num2, num3) {
   // La función recibe tres números distintos.
   // Si num1 es mayor a num2 y a num3, y además es positivo, retorna ---> "Numero 1 es mayor y positivo".
-  // Si alguno de los tres números es negativo, retorna ---> "Hay negativos".
+  // Si alguno de los tres números es negativo, retorna ---> "Hay negativos". 
   // Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retorna el nuevo valor.
   // Si todos los argumentos son cero, retorna ---> "Error".
   // Si no se cumple ninguna de las condiciones anteriores, retorna false.
   // Tu código:
+
+  // if () {
+
+  // } else if () {
+
+  // } else if () {
+    
+  // } else if () {
+
+  // } else {
+
+  // }
+  
+  if (num1 >= 0 && num1 > num3 && num1 > num2) {
+    return "Numero 1 es mayor y positivo";
+  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  } else if (num3 > num1 && num3 > num2 ) {
+      return num3 += 1;
+  } else if (num1, num2, num3 === 0) {
+      return "Error";
+  } else {
+      return false;
+  }
+  
+  // condition1
+  // num1 > num2 && num1 > num3 && num1 >= 0
+  // "Numero 1 es mayor y positivo";
+  
+  // condition2
+  // num1, num2, num3 === < 0
+  // "Hay negativos"; 
+  
+  // condition3
+  //   num3 > num1 && num2 
+  //   num3 =+ 1; 
+  
+  // condition4
+  //   num1, num2, num3 === 0;
+  //   "Error"
+  
+  //   else
+  // false
+  
 }
+// num1 = 0;
+// num2 = 0;
+// num3 = 0;
+
+// console.log(operadoresLogicos(num1, num2, num3));
 
 module.exports = operadoresLogicos;
--- M07 Arrays/ejercicios/04.js
+++ M07 Arrays/ejercicios/04.js
@@ -2,6 +2,10 @@ function obtenerElementoAleatorio(array) {
   // Devuelve un elemento aleatorio del arreglo array.
   // PISTA: Usa el método Math.random().
   // Tu código:
+  console.log(Math.random(array.length));
 }
+
+let array = [1,2,3,4,5,6];
+obtenerElementoAleatorio(array);
 
 module.exports = obtenerElementoAleatorio;
